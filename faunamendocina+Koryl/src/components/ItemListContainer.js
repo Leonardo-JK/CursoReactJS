3d785ac582220275098b49680598b4d5747a9588
Leonardo-JK <leok_86@hotmail.com>
2022-03-19 18:31:44 -0300

    correccion

--- faunamendocina+Koryl/src/components/ItemListContainer.js
+++ faunamendocina+Koryl/src/components/ItemListContainer.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ItemList from './ItemList';
-import data from '../data/stock';
+
+
+let info;
+console.log("inicio");
+const datos = new Promise((resolve, reject) => {
+      resolve(true);  
+});
+  
+datos.then(result => {info = (require('../data/stock.json'))});
+
 
 function ItemListContainer() {
     return (
         <div className='products'>
-            {data.map((dat) => {
+            {info.map((dat) => {
                 return (
                     <div key={dat.categoria} className='products__list'>
                         <h2 className='products__title'>{dat.categoria}</h2>
